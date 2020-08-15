281d8719e278fb86ff85386acba7d7a2e248db71
eltoto1219 <antonio36764@gmail.com>
2020-08-14 23:32:46 -0400

    modified gqa

--- src/tasks/gqa.py
+++ src/tasks/gqa.py
@@ -10,22 +10,24 @@ import torch.nn as nn
 from torch.utils.data.dataloader import DataLoader
 
 from param import args
-from pretrain.qa_answer_table import load_lxmert_qa
-from tasks.gqa_model import GQAModel
 from tasks.gqa_data import GQADataset, GQATorchDataset, GQAEvaluator
+from transformers import LxmertForQuestionAnswering, LxmertConfig, LxmertTokenizer
 
 
-DataTuple = collections.namedtuple("DataTuple", 'dataset loader evaluator')
+DataTuple = collections.namedtuple("DataTuple", "dataset loader evaluator")
 
 
-def get_tuple(splits: str, bs:int, shuffle=False, drop_last=False) -> DataTuple:
+def get_tuple(splits: str, bs: int, shuffle=False, drop_last=False) -> DataTuple:
     dset = GQADataset(splits)
     tset = GQATorchDataset(dset)
     evaluator = GQAEvaluator(dset)
     data_loader = DataLoader(
-        tset, batch_size=bs,
-        shuffle=shuffle, num_workers=args.num_workers,
-        drop_last=drop_last, pin_memory=True
+        tset,
+        batch_size=bs,
+        shuffle=shuffle,
+        num_workers=args.num_workers,
+        drop_last=drop_last,
+        pin_memory=True,
     )
 
     return DataTuple(dataset=dset, loader=data_loader, evaluator=evaluator)
@@ -33,44 +35,36 @@ def get_tuple(splits: str, bs:int, shuffle=False, drop_last=False) -> DataTuple:
 
 class GQA:
     def __init__(self):
+        # load the model
+        self.tokenizer = LxmertTokenizer.from_pretrained("unc-nlp/lxmert-base-uncased", cache_dir="cache", force_download = True)
+        self.model = LxmertForQuestionAnswering.from_pretrained("unc-nlp/lxmert-base-uncased", cache_dir="cache", force_download = True, num_qa_labels=self.train_tuple.dataset.num_answers)
+        # load the data
         self.train_tuple = get_tuple(
             args.train, bs=args.batch_size, shuffle=True, drop_last=True
         )
         if args.valid != "":
-            valid_bsize = 2048 if args.multiGPU else 512
+            valid_bsize = 32 if args.multiGPU else 512
             self.valid_tuple = get_tuple(
-                args.valid, bs=valid_bsize,
-                shuffle=False, drop_last=False
+                args.valid, bs=valid_bsize, shuffle=False, drop_last=False
             )
         else:
             self.valid_tuple = None
-
-        self.model = GQAModel(self.train_tuple.dataset.num_answers)
-
         # Load pre-trained weights
-        if args.load_lxmert is not None:
-            self.model.lxrt_encoder.load(args.load_lxmert)
-        if args.load_lxmert_qa is not None:
-            load_lxmert_qa(args.load_lxmert_qa, self.model,
-                           label2ans=self.train_tuple.dataset.label2ans)
-
         # GPU options
         self.model = self.model.cuda()
-        if args.multiGPU:
-            self.model.lxrt_encoder.multi_gpu()
 
         # Losses and optimizer
         self.bce_loss = nn.BCEWithLogitsLoss()
-        self.mce_loss = nn.CrossEntropyLoss(ignore_index=-1)
-        if 'bert' in args.optim:
+        self.mce_loss = nn.CrossEntropyLoss()
+        if "bert" in args.optim:
             batch_per_epoch = len(self.train_tuple.loader)
             t_total = int(batch_per_epoch * args.epochs)
             print("Total Iters: %d" % t_total)
             from lxrt.optimization import BertAdam
-            self.optim = BertAdam(list(self.model.parameters()),
-                                  lr=args.lr,
-                                  warmup=0.1,
-                                  t_total=t_total)
+
+            self.optim = BertAdam(
+                list(self.model.parameters()), lr=args.lr, warmup=0.1, t_total=t_total
+            )
         else:
             self.optim = args.optimizer(list(self.model.parameters()), args.lr)
 
@@ -79,28 +73,50 @@ class GQA:
 
     def train(self, train_tuple, eval_tuple):
         dset, loader, evaluator = train_tuple
-        iter_wrapper = (lambda x: tqdm(x, total=len(loader))) if args.tqdm else (lambda x: x)
+        iter_wrapper = (
+            (lambda x: tqdm(x, total=len(loader))) if args.tqdm else (lambda x: x)
+        )
 
-        best_valid = 0.
+        best_valid = 0.0
         for epoch in range(args.epochs):
             quesid2ans = {}
-            for i, (ques_id, feats, boxes, sent, target) in iter_wrapper(enumerate(loader)):
+            for i, (ques_id, feats, boxes, sent, target) in iter_wrapper(
+                enumerate(loader)
+            ):
 
                 self.model.train()
                 self.optim.zero_grad()
 
                 feats, boxes, target = feats.cuda(), boxes.cuda(), target.cuda()
-                logit = self.model(feats, boxes, sent)
-                assert logit.dim() == target.dim() == 2
-                if args.mce_loss:
-                    max_value, target = target.max(1)
-                    loss = self.mce_loss(logit, target) * logit.size(1)
-                else:
-                    loss = self.bce_loss(logit, target)
-                    loss = loss * logit.size(1)
+                inputs = self.tokenizer(
+                    sent,
+                    padding="max_length",
+                    max_length=20,
+                    truncation=True,
+                    return_token_type_ids=True,
+                    return_attention_mask=True,
+                    add_special_tokens=True
+                )
+                token_type_ids=torch.tensor(inputs.token_type_ids).cuda()
+                input_ids = torch.tensor(inputs.input_ids).cuda()
+                attention_mask = torch.tensor(inputs.attention_mask).cuda()
+                print(token_type_ids, input_ids, attention_mask)
+                max_value, target = target.max(1)
+                output = self.model(input_ids=input_ids, visual_feats=feats, visual_pos=boxes, labels=target, token_type_ids=token_type_ids, return_dict=True, output_attentions=False)
+                loss = output.loss
+                logit = output.question_answering_score
+
+
+                #assert logit.dim() == target.dim() == 2
+                #if args.mce_loss:
+                #    max_value, target = target.max(1)
+                #    loss = self.mce_loss(logit, target) * logit.size(1)
+                #else:
+                #    loss = self.bce_loss(logit, target)
+                #    loss = loss * logit.size(1)
 
                 loss.backward()
-                nn.utils.clip_grad_norm_(self.model.parameters(), 5.)
+                nn.utils.clip_grad_norm_(self.model.parameters(), 5.0)
                 self.optim.step()
 
                 score, label = logit.max(1)
@@ -108,7 +124,10 @@ class GQA:
                     ans = dset.label2ans[l]
                     quesid2ans[qid] = ans
 
-            log_str = "\nEpoch %d: Train %0.2f\n" % (epoch, evaluator.evaluate(quesid2ans) * 100.)
+            log_str = "\nEpoch %d: Train %0.2f\n" % (
+                epoch,
+                evaluator.evaluate(quesid2ans) * 100.0,
+            )
 
             if self.valid_tuple is not None:  # Do Validation
                 valid_score = self.evaluate(eval_tuple)
@@ -116,30 +135,39 @@ class GQA:
                     best_valid = valid_score
                     self.save("BEST")
 
-                log_str += "Epoch %d: Valid %0.2f\n" % (epoch, valid_score * 100.) + \
-                           "Epoch %d: Best %0.2f\n" % (epoch, best_valid * 100.)
+                log_str += "Epoch %d: Valid %0.2f\n" % (
+                    epoch,
+                    valid_score * 100.0,
+                ) + "Epoch %d: Best %0.2f\n" % (epoch, best_valid * 100.0)
 
-            print(log_str, end='')
+            print(log_str, end="")
 
-            with open(self.output + "/log.log", 'a') as f:
+            with open(self.output + "/log_test.log", "a") as f:
                 f.write(log_str)
                 f.flush()
 
-        self.save("LAST")
+        self.save("WOOT")
 
     def predict(self, eval_tuple: DataTuple, dump=None):
         self.model.eval()
         dset, loader, evaluator = eval_tuple
         quesid2ans = {}
         for i, datum_tuple in enumerate(loader):
-            ques_id, feats, boxes, sent = datum_tuple[:4]   # avoid handling target
+            ques_id, feats, boxes, sent, target = datum_tuple[:5]  # avoid handling target
             with torch.no_grad():
                 feats, boxes = feats.cuda(), boxes.cuda()
-                logit = self.model(feats, boxes, sent)
+                inputs = self.tokenizer(sent, padding="max_length", max_length=20, truncation=True)
+                token_type_ids=torch.tensor(inputs.token_type_ids).cuda()
+                input_ids = torch.tensor(inputs.input_ids).cuda()
+                #max_value, target = target.max(1)
+                output = self.model(input_ids=input_ids, visual_feats=feats, visual_pos=boxes, token_type_ids=token_type_ids, return_dict=True)
+                loss = output.loss
+                logit = output.question_answering_score
                 score, label = logit.max(1)
                 for qid, l in zip(ques_id, label.cpu().numpy()):
                     ans = dset.label2ans[l]
                     quesid2ans[qid] = ans
+
         if dump is not None:
             evaluator.dump_result(quesid2ans, dump)
         return quesid2ans
@@ -161,50 +189,51 @@ class GQA:
         return evaluator.evaluate(quesid2ans)
 
     def save(self, name):
-        torch.save(self.model.state_dict(),
-                   os.path.join(self.output, "%s.pth" % name))
+        torch.save(self.model.state_dict(), os.path.join(self.output, "%s.pth" % name))
 
     def load(self, path):
         print("Load model from %s" % path)
         state_dict = torch.load("%s.pth" % path)
         for key in list(state_dict.keys()):
-            if '.module' in key:
-                state_dict[key.replace('.module', '')] = state_dict.pop(key)
+            if ".module" in key:
+                state_dict[key.replace(".module", "")] = state_dict.pop(key)
         self.model.load_state_dict(state_dict, strict=False)
 
 
 if __name__ == "__main__":
     # Build Class
+    #conf = LxmertConfig(num_qa_labels=1536)
+    #model = LxmertForQuestionAnswering(conf)
+    #model = model.from_pretrained("unc-nlp/lxmert-base-uncased", cache_dir="ssd-playpen/avmendoz")
+    #gqa = LxmertForQuestionAnswering.from_pretrained("unc-nlp/lxmert-base-uncased", cache_dir="ssd-playpen/avmendoz")
     gqa = GQA()
-
-    # Load Model
-    if args.load is not None:
-        gqa.load(args.load)
-
     # Test or Train
     if args.test is not None:
-        args.fast = args.tiny = False       # Always loading all data in test
-        if 'submit' in args.test:
+        args.fast = args.tiny = False  # Always loading all data in test
+        if "submit" in args.test:
+            raise Exception("not testing")
             gqa.predict(
-                get_tuple(args.test, bs=args.batch_size,
-                          shuffle=False, drop_last=False),
-                dump=os.path.join(args.output, 'submit_predict.json')
+                get_tuple(
+                    args.test, bs=args.batch_size, shuffle=False, drop_last=False
+                ),
+                dump=os.path.join(args.output, "submit_predict.json"),
             )
-        if 'testdev' in args.test:
+        else:
             result = gqa.evaluate(
-                get_tuple('testdev', bs=args.batch_size,
-                          shuffle=False, drop_last=False),
-                dump=os.path.join(args.output, 'testdev_predict.json')
+                get_tuple(
+                    "valid", bs=args.batch_size, shuffle=False, drop_last=False
+                ),
+                dump=os.path.join(args.output, "testdev_predict_aug.json"),
             )
             print(result)
     else:
-        # print("Train Oracle: %0.2f" % (gqa.oracle_score(gqa.train_tuple) * 100))
-        print('Splits in Train data:', gqa.train_tuple.dataset.splits)
+        # raise Exception("model is not testing")
+        print("Train Oracle: %0.2f" % (gqa.oracle_score(gqa.train_tuple) * 100))
+        print("Splits in Train data:", gqa.train_tuple.dataset.splits)
         if gqa.valid_tuple is not None:
-            print('Splits in Valid data:', gqa.valid_tuple.dataset.splits)
+            print("whao")
+            print("Splits in Valid data:", gqa.valid_tuple.dataset.splits)
             print("Valid Oracle: %0.2f" % (gqa.oracle_score(gqa.valid_tuple) * 100))
         else:
             print("DO NOT USE VALIDATION")
         gqa.train(gqa.train_tuple, gqa.valid_tuple)
-
-
